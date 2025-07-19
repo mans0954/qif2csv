b4ceb915897839fee61d4266d56f0dbb3dc6bf41
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-07-19 12:02:18 +0100

    Make filenames configurable

--- qif2csv.py
+++ qif2csv.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
 from quiffen import Qif, QifDataType
+import argparse
 import decimal
 
-qif = Qif.parse('input.qif', day_first=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", default='input.qif', help="input QIF file")
+parser.add_argument("--output", default='output.csv', help="output CSV file")
+
+args, unknown_args = parser.parse_known_args()
+
+print(args)
+
+qif = Qif.parse(args.input, day_first=True)
 df = qif.to_dataframe(data_type=QifDataType.TRANSACTIONS)
-df.to_csv('output.csv', index=False)
+df.to_csv(args.output, index=False)
