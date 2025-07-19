bb9eb4e93edd051842b6c12ff4797d6106c0bfaa
Christopher Hoskin <mans0954@users.noreply.github.com>
2025-07-19 13:53:25 +0100

    Output to ODS instead

--- qif2csv.py
+++ qif2csv.py
@@ -2,16 +2,17 @@
 
 from quiffen import Qif, QifDataType
 import argparse
-import decimal
+import pandas
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", default='input.qif', help="input QIF file")
-parser.add_argument("--output", default='output.csv', help="output CSV file")
+parser.add_argument("--output", default='output.ods', help="output ODS file")
 
 args, unknown_args = parser.parse_known_args()
 
-print(args)
-
 qif = Qif.parse(args.input, day_first=True)
 df = qif.to_dataframe(data_type=QifDataType.TRANSACTIONS)
-df.to_csv(args.output, index=False, columns=['date', 'payee', 'amount', 'line_number'])
+#df.to_csv(args.output, index=False, columns=['date', 'payee', 'amount', 'line_number'])
+
+df.to_excel(args.output, index=False, sheet_name=os.path.basename(args.output), columns=['date', 'payee', 'amount', 'line_number'])
